1e8420bc0e575ba9e77d7f0fb4672cef400211e3
marcogrimme <mgrimme954@gmail.com>
2024-05-11 09:49:18 +0200

    funktion eindeutiger benannt und klasse umbenannt

--- src/main/kotlin/SpellMaster.kt
+++ src/main/kotlin/SpellMaster.kt
@@ -4,40 +4,40 @@ import javax.sound.sampled.AudioSystem
 import javax.sound.sampled.Clip
 import javax.sound.sampled.FloatControl
 
-open class Action {
-    open val zaubersprueche: MutableMap<String, Int> = mutableMapOf(
+open class SpellMaster {
+    open val magicSpellMap: MutableMap<String, Int> = mutableMapOf(
         "expecto patronum" to  50,
         "stupor" to  100,
         "expelliarmus" to  150,
         "avada kedavra" to  200
     )
 //Hier wird der Schaden von der map genommen man gibt den key ein und bekommt den Value wert als zauberSpruchschaden und man hat 3 versuche es wird über die readline eingeben
-    open fun zauberSpruchAnwenden(zauberSpruchName: String): Int? {
-        var zauberSpruchSchaden = zaubersprueche[zauberSpruchName]
-        var versuche = 1
+    open fun applySpell(spellName: String): Int? {
+        var spellDamage = magicSpellMap[spellName]
+        var attempts = 1
 
-        while (zauberSpruchSchaden == null && versuche <= 3) {
+        while (spellDamage == null && attempts <= 3) {
             println("Bitte gib einen gültigen Zauberspruch ein: ")
             val benutzereingabe = readln()
-            zauberSpruchSchaden = zaubersprueche[benutzereingabe]
-            versuche++
+            spellDamage = magicSpellMap[benutzereingabe]
+            attempts++
 
-            if (zauberSpruchSchaden == null && versuche <= 3) {
-                println("Zauber: $benutzereingabe nicht gefunden. Versuch $versuche von 3.")
+            if (spellDamage == null && attempts <= 3) {
+                println("Zauber: $benutzereingabe nicht gefunden. Versuch $attempts von 3.")
             }
         }
-        if (zauberSpruchSchaden == null) {
+        if (spellDamage == null) {
             println("Drei Versuche wurden erreicht. Angriff wurde abgebrochen.")
         } else {
-            println("${blue}Zauber: $zauberSpruchName, Schaden: $zauberSpruchSchaden hP$reset")
+            println("${blue}Zauber: $spellName, Schaden: $spellDamage hP$reset")
         }
-        return zauberSpruchSchaden
+        return spellDamage
     }
 }
 
 
 //Außerhalb der klasse
-fun titelUeberschrift(){
+fun printGameTitle(){
     println(
         "$yellow                                        _ __\n" +
                 "        ___                             | '  \\\n" +
@@ -56,16 +56,11 @@ fun titelUeberschrift(){
     println()
     println("      ----Der Kampf Beginnt----")
 }
-fun audio (audiopath : String) {
-
-    val audio: File = File(audiopath)
-
+fun playAudio (audioPath : String) {
+    val audio: File = File(audioPath)
     val audioInput: AudioInputStream = AudioSystem.getAudioInputStream(audio)
-
     val clip: Clip = AudioSystem.getClip()
-
     clip.open(audioInput)
-
     clip.start()
 
     if (clip.isControlSupported(FloatControl.Type.MASTER_GAIN)) {
@@ -76,22 +71,18 @@ fun audio (audiopath : String) {
         println("Master Gain Control not Supported")
     }
 }
-fun gameMusik(){
-    val audiopath = "Sounds/Hedwig's Theme.wav"
-    audio(audiopath)
+fun playGameMusic(){
+    val audioPath = "Sounds/Hedwig's Theme.wav"
+    playAudio(audioPath)
 }
 
-//Gobale variabel
-val red = "\u001B[31m"
-val green = "\u001B[32m"
-val yellow = "\u001B[33m"
-val blue = "\u001B[34m"
-val magenta = "\u001B[35m"
-val cyan = "\u001B[36m"
-val bold = "\u001B[1m"
-val underline = "\u001B[4m"
-val backgroundYellow = "\u001B[43m"
-val reset = "\u001B[0m"
+// variabel Farben
+const val red = "\u001B[31m"
+const val green = "\u001B[32m"
+const val yellow = "\u001B[33m"
+const val blue = "\u001B[34m"
+const val magenta = "\u001B[35m"
+const val reset = "\u001B[0m"
 
 
 
