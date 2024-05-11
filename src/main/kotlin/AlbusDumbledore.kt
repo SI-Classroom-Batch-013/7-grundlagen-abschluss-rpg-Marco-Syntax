00719dfed11c09b2b1e96d9a888e5b73439a773b
marcogrimme <mgrimme954@gmail.com>
2024-05-11 09:35:44 +0200

    funktion eindeutiger benannt

--- src/main/kotlin/AlbusDumbledore.kt
+++ src/main/kotlin/AlbusDumbledore.kt
@@ -1,11 +1,11 @@
 open class AlbusDumbledore(name: String, hp: Int, action: Action) : Zauberer(name, hp, Action()) {
 
     //Fawkes attacke anwenden
-    fun fawkesAttacke(ziel: DunklerZauberer) {
+    fun phoenixStrike(ziel: DarkMage) {
         println("$name hat sein Phönix FAWKES gerufen und führt seine Attacke Feuerball aus")
-        val feuerball: Int = 100
-        ziel.hp -= feuerball
-        println("Die Attacke hat $feuerball schaden verursacht bei ${ziel.name}")
+        val phoenixStrike: Int = 100
+        ziel.hp -= phoenixStrike
+        println("Die Attacke hat $phoenixStrike schaden verursacht bei ${ziel.name}")
     }
 
 }
