3b4606cf086d07f9e5722f74fd1e036be8eeb64a
marcogrimme <mgrimme954@gmail.com>
2024-05-11 10:21:18 +0200

    funktion eindeutiger benannt

--- src/main/kotlin/RonWesley.kt
+++ src/main/kotlin/RonWesley.kt
@@ -1,14 +1,11 @@
-open class RonWesley(name: String, hp: Int, action: Action) : Zauberer(name, hp, Action()) {
+open class RonWesley(name: String, hp: Int, action: SpellMaster) : Wizard(name, hp, SpellMaster()) {
 
     //ruf seine Ratte Krätze und kann extra schaden verursachen
-
-    fun ratteKrätze(ziel: DunklerZauberer) {
+    fun ratAttack(ziel: DarkMage) {
         println("$name hat sein Wandlungzauber (100 Punkte schaden) angewendet seine Attacke wird um 50 Schadenspunkte verstärkt durch seine Ratte Krätze")
         val wandlungZauber: Int = 50
         ziel.hp -= wandlungZauber + 100
         println("Die Attacke hat ${wandlungZauber + 100} schaden verursacht")
         println("${ziel.name} hat noch ${ziel.hp} Lebenspunkte")
     }
-
-
 }
